2fd6fc75291217c550f94e1703c4e236e2577618
Adarshrajsrivastwa <srivastwaadarsh@gmail.com>
2025-03-27 21:01:19 +0530

    update club route

--- app.js
+++ app.js
@@ -17,13 +17,14 @@ const cookieParser = require("cookie-parser");
 const usermodels = require('./models/usermodels');
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+let club=require('./routes/Club.js');
 
 
 dotenv.config();
 connectdb();
 
 const app = express();
-const PORT =  4000;
+const PORT =   3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -50,6 +51,7 @@ app.use(cors());
 
 require("./config/google_oauth_config");
 app.use("/auth", authRouter);
+app.use("/club",club)
 
 
 
@@ -300,83 +302,7 @@ app.get('/gallery', (req, res) => {
 });
 
 // Coding Club route
-app.get('/coding', (req, res) => {
-  res.render('Coding club', { title: 'Coding Club Page' });
-});
 
-app.get('/Quiz ', (req, res) => {
-  res.render('Anwesha Event/Technical Quiz ', { title: 'Quiz Page' });
-})
-
-// Drone Club route
-app.get('/drone',  (req, res) => {
-  res.render('drone club', { title: 'Drone Club Page' });
-});
-
-// Media Fusion route
-app.get('/media', (req, res) => {
-  res.render('mediafusion', { title: 'Media Fusion Page' });
-});
-
-// Cultural Club route
-app.get('/Cultrul',  (req, res) => {
-  res.render('Cultrul', { title: 'Cultural Club Page' });
-});
-
-// Robotics Club route
-app.get('/Robotics',  (req, res) => {
-  res.render('Robotics', { title: 'Robotics Club Page' });
-});
-
-// Entrepreneurship Club route
-app.get('/entrepreneurship',  (req, res) => {
-  res.render('entrepreneurship club', { title: 'Entrepreneurship Club Page' });
-});
-
-// Literary Club route
-app.get('/Literary',  (req, res) => {
-  res.render('Literary club', { title: 'Literary Club Page' });
-});
-
-// Renewable Energy route
-app.get('/Renewable',  (req, res) => {
-  res.render('Renewable energy', { title: 'Renewable Energy Page' });
-});
-
-// Discipline Club route
-app.get('/Disipline',  (req, res) => {
-  res.render('Disipline club', { title: 'Discipline Club Page' });
-});
-
-// Alumni Relation Club route
-app.get('/Alimni',  (req, res) => {
-  res.render('Alimni relation club', { title: 'Alumni Relation Club Page' });
-});
-
-// Tech Wizards route
-app.get('/Tech',  (req, res) => {
-  res.render('Tech wizards', { title: 'Tech Wizards Club Page' });
-});
-
-// EV Club route
-app.get('/EV',  (req, res) => {
-  res.render('EV', { title: 'EV Club Page' });
-});
-
-// Music Club route
-app.get('/music',  (req, res) => {
-  res.render('music', { title: 'Music Club Page' });
-});
-
-// Dance Club route
-app.get('/Dance',  (req, res) => {
-  res.render('Dance', { title: 'Dance Club Page' });
-});
-
-// IEEE Student Chapter route
-app.get('/IEEE',  (req, res) => {
-  res.render('IEEE Student chapter', { title: 'IEEE Student Chapter Page' });
-});
 
 // Developer route
 app.get('/Dev',  (req, res) => {
