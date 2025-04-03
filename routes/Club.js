d7b0dcd88925e863ded4d934f9005f211c8c1934
Krishna Jadhav <krishnajadhav85017@gmail.com>
2025-04-04 00:46:49 +0530

    dynamic club

--- routes/Club.js
+++ routes/Club.js
@@ -1,15 +1,11 @@
 let express = require('express');
 let app = express();
-const clubs = require("../data/club.data").clubs; // Access the clubs array
+const clubs = require("../data/club.data").clubs;
 const { isLoggedIn } = require("../middleware/isLoggedIn");
 
 app.get('/:clubName', (req, res) => {
   const clubName = req.params.clubName.toLowerCase();
-  console.log(clubName);
-
-  const club = clubs.find(club => club.name.toLowerCase() === clubName); // Find the club by name
-  console.log(club);
-
+  const club = clubs.find(club => club.name.toLowerCase() === clubName);
   if (club) {
     res.render('club', { club });
   } else {
