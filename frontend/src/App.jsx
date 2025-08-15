620df5f37733e14807a6de0d6e0537b0deaa341e
Rishabh Tomar <rishabhtomar9999@gmail.com>
2025-08-15 15:55:13 +0530

    setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello, Council Website!
+      </h1>
+      <p>Welcome to your new React app.</p>
+    </div>
+  );
+}
+
+export default App;
