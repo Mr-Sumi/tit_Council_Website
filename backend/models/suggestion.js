de71249f276f7d4a38d29917d1a7ad1e3fa2c3c4
Adarshrajsrivastwa <srivastwaadarsh@gmail.com>
2025-08-20 21:55:33 +0530

    update db

--- backend/models/suggestion.js
+++ backend/models/suggestion.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const suggestionSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, trim: true, minlength: 2, maxlength: 100 },
-    enrollment: { type: String, required: true, trim: true, match: [/^[A-Za-z0-9-]+$/, "Invalid enrollment format"] },
-    college: { type: String, required: true, trim: true, maxlength: 200 },
+    enrollment: { type: String, required: true, trim: true},
+    college: { type: String, required: true, trim: true},
     problem: { type: String, required: true, trim: true, minlength: 10, maxlength: 2000 },
     solution: { type: String, required: true, trim: true, minlength: 5, maxlength: 2000 },
   },
